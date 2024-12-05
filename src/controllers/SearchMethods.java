dc1dfb81b19985f00a6c530b16db0575115748c9
alekspaucar <alexpaucar.887@gmail.com>
2024-12-04 19:50:16 -0500

    Update SearchMethods.java

--- src/controllers/SearchMethods.java
+++ src/controllers/SearchMethods.java
@@ -1,5 +1,36 @@
 package controllers;
 
+import models.Person;
+
 public class SearchMethods {
-    
+
+    public int sequentialSearchByName(Person[] personas, String nombre) {
+        for (int i = 0; i < personas.length; i++) {
+            if (personas[i].getName().equalsIgnoreCase(nombre)) {
+                return i; // Retorna el índice donde se encuentra la persona.
+            }
+        }
+        return -1; // Retorna -1 si no se encuentra.
+    }
+
+    public int binarySearchByName(Person[] personas, String nombre) {
+        int inicio = 0;
+        int fin = personas.length - 1;
+
+        while (inicio <= fin) {
+            int medio = inicio + (fin - inicio) / 2;
+            int comparacion = personas[medio].getName().compareToIgnoreCase(nombre);
+
+            if (comparacion == 0) {
+                return medio; // Retorna el índice donde se encuentra la persona.
+            }
+
+            if (comparacion < 0) {
+                inicio = medio + 1; // Busca en la mitad derecha.
+            } else {
+                fin = medio - 1; // Busca en la mitad izquierda.
+            }
+        }
+        return -1; // Retorna -1 si no se encuentra.
+    }
 }
