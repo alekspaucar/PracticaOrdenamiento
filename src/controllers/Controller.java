c85abc105c2d3c373deb01adcd9c5dca851a05b3
alekspaucar <alexpaucar.887@gmail.com>
2024-12-04 19:46:54 -0500

    corregido

--- src/controllers/Controller.java
+++ src/controllers/Controller.java
@@ -12,12 +12,10 @@ public class Controller {
     private Person[] personas;
 
     public Controller(View view, ShortingMethods shortingMethods, SearchMethods searchMethods) {
-
         this.view = view;
         this.shortingMethods = shortingMethods;
         this.searchMethod = searchMethods;
         System.out.println("controller creator");
-
     }
 
     public void start() {
@@ -27,62 +25,61 @@ public class Controller {
             switch (option) {
                 case 1:
                     inputPersonas();
+                    break;
                 case 2:
                     addPerson();
-                case 3: 
+                    break;
+                case 3:
                     sortPerson();
+                    break;
                 case 100:
-                    System.out.println("adioss");
+                    System.out.println("Adiós");
                     break;
                 default:
-                    System.out.println("opcion no valida");
+                    System.out.println("Opción no válida");
             }
-
-        } while (option != 0);
+        } while (option != 100);
     }
 
     public void inputPersonas() {
-        int numeroPersonas = view.inputInt("ingrese el numero de personas: ");
+        int numeroPersonas = view.inputInt("Ingrese el número de personas: ");
         personas = new Person[numeroPersonas];
 
         for (int i = 0; i < numeroPersonas; i++) {
             personas[i] = view.inputPerson();
-
         }
-
     }
 
     public void addPerson() {
-        if (personas == null){
+        if (personas == null) {
+            view.showMensajae("No hay datos, ingresando nuevas personas.");
             inputPersonas();
-            view.showMensajae("no hay datos:");
-            }else{
-                    int numeroPersonas = view.inputInt("ingrese el numero de personas a adicionar: ");
+        } else {
+            int numeroPersonas = view.inputInt("Ingrese el número de personas a adicionar: ");
+            Person[] personasTotales = new Person[personas.length + numeroPersonas];
 
-                    Person[] personasTotales = new Person[personas.length + numeroPersonas];
-
-                    for (int i = 0; i < personas.length; i++) {
-                        personasTotales[i] = personas[i];
-
-                    }
-                    for (int i = personas.length; i < personasTotales.length; i++) {
-                        personas[i] = view.inputPerson();
-
-                    }
-                    personas = personasTotales;
-        
-    }
-        public void sortPerson(){
-
-            int sortingOption = view.selectSortingMethod();
-                
-            if (sortingOption==1){
-                shortingMethods.sortByNameWithBuble(personas);
-            }else if(sortingOption==2) {
-                shortingMethods.sortByNameWithSelectio(personas)
-            }else{
-                view.showMensajae("opcion no valida");
+            for (int i = 0; i < personas.length; i++) {
+                personasTotales[i] = personas[i];
             }
+
+            for (int i = personas.length; i < personasTotales.length; i++) {
+                personasTotales[i] = view.inputPerson();
+            }
+
+            personas = personasTotales;
+        }
     }
 
+    public void sortPerson() {
+        int sortingOption = view.selectSortingMethod();
+
+        if (sortingOption == 1) {
+            shortingMethods.sortByNameWithBuble(personas);
+        } else if (sortingOption == 2) {
+            shortingMethods.sortByNameWithSelectio(personas);
+        } else {
+            view.showMensajae("Opción no válida");
+        }
+    }
 }
+
